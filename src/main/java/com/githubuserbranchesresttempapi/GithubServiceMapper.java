0bb1049e485ed075a739967e354091f4bf315f3d
Maciej Mysliwiec <maciej.m.mysliwiec@gmail.com>
2023-09-13 18:52:14 +0200

    refactored the GithubServiceMapper class and UserNameResponseDto record

--- src/main/java/com/githubuserbranchesresttempapi/GithubServiceMapper.java
+++ src/main/java/com/githubuserbranchesresttempapi/GithubServiceMapper.java
@@ -1,8 +1,15 @@
 package com.githubuserbranchesresttempapi;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.githubuserbranchesresttempapi.dto.UserNameResponseDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
 
 @Component
 @Log4j2
@@ -11,5 +18,16 @@ public class GithubServiceMapper {
 
     public GithubServiceMapper(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
+    }
+
+    public List<UserNameResponseDto> mapJsontoUserNameResponseDtoList(String json) {
+        try {
+            TypeReference<List<UserNameResponseDto>> typeReference = new TypeReference<>() {
+            };
+            return objectMapper.readValue(json, typeReference);
+        } catch (JsonProcessingException e) {
+            log.error("GithubSeviceMapper could not map json", e.getMessage());
+            return Collections.emptyList();
+        }
     }
 }
